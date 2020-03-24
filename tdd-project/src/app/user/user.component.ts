b7105ed7e5587631b76708986d9165993a93d131
ozoloko <contato.matheusluiz@gmail.com>
2020-03-24 20:49:32 -0300

    objetos ts

--- tdd-project/src/app/user/user.component.ts
+++ tdd-project/src/app/user/user.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  user: {name: string};
+  isLoggedIn = false;
+
   constructor() { }
 
   ngOnInit(): void {
