dc6ae1dd8e8b802b9ae5df8d4b8a7e0becbda944
ozoloko <contato.matheusluiz@gmail.com>
2020-03-24 21:30:43 -0300

    Criação de teste com serviço synchronous https://bit.ly/3bpDgmi

--- tdd-project/src/app/user/user.component.spec.ts
+++ tdd-project/src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   describe('Component: User', () => {
@@ -17,6 +18,31 @@ describe('UserComponent', () => {
       let app = fixture.debugElement.componentInstance;
 
       expect(app).toBeTruthy();
+    })
+
+    it('Deve pegar o nome do usuário do serviço', () => {
+      let fixture = TestBed.createComponent(UserComponent); // Cria o componente
+      let app = fixture.debugElement.componentInstance;
+      let userService = fixture.debugElement.injector.get(UserService); // pega as injeções de dependência do componente no .injector e .get para trazer a instancia do serviço desejado
+      fixture.detectChanges(); //Detecta alguma mudança no estado do componente. Isso roda automáticamente no angular, mas não no teste. Caso esqueça isso o teste falha, pois user.name começou undefined
+      expect(userService.user.name).toEqual(app.user.name); // Espera que o nome do objeto no serviço seja o mesmo da variável no componente
+    })
+    
+    it('Deve mostrar o nome do usuário se ele estiver logado', () => {
+      let fixture = TestBed.createComponent(UserComponent);
+      let app = fixture.debugElement.componentInstance;
+      app.isLoggedIn = true; // Variável para true como se user estivesse logado
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement; // Pega o html da instancia do componente
+      expect(compiled.querySelector('p').textContent).toContain(app.user.name); // <p> deve ter o mesmo conteúdo da variável com o nome
+    })
+
+    it('Não deve mostrar o nome do usuário se ele estiver deslogado', () => {
+      let fixture = TestBed.createComponent(UserComponent);
+      let app = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('p').textContent).not.toContain(app.user.name); // <p> não deve conter o conteúdo da variável
     })
   });
   
